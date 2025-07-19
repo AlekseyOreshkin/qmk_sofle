5acfcb655c8db442e6117530ff6257b34eaa68a6
ALNOR-HPC\alnor <alxrsk@gmail.com>
2025-07-19 10:06:08 +0300

    обновление раскладки

--- keyboards/sofle/keymaps/custom/keymap.c
+++ keyboards/sofle/keymaps/custom/keymap.c
@@ -4,10 +4,10 @@
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_LEFT,       KC_RGHT),     ENCODER_CCW_CW(KC_DOWN,       KC_UP) },
-    [1] = { ENCODER_CCW_CW(KC_MS_WH_DOWN, KC_MS_WH_UP), ENCODER_CCW_CW(KC_MS_WH_LEFT, KC_MS_WH_RIGHT) },
-    [2] = { ENCODER_CCW_CW(_______, _______),           ENCODER_CCW_CW(_______, _______) },
-    [3] = { ENCODER_CCW_CW(_______, _______),           ENCODER_CCW_CW(_______, _______) }
+    [0] = { ENCODER_CCW_CW(KC_VOLU, KC_VOLD), ENCODER_CCW_CW(MS_WHLU, MS_WHLD) },
+    [1] = { ENCODER_CCW_CW(KC_MPRV, KC_MNXT), ENCODER_CCW_CW(MS_WHLL, MS_WHLR) },
+    [2] = { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
+    [3] = { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) }
 };
 #endif
 
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_RBRC,
           TD_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_LBRC,
           KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-          KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_PGUP,  KC_PGDN,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+          KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_MUTE,  MS_BTN2,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
                  KC_LCTL, KC_LALT, KC_LGUI, TL_LOWR, KC_SPC,                      KC_ENT, TL_UPPR,  KC_APP,  KC_RALT, KC_RCTL
   // ╰──────────────────────────────────────────────────────╯                   ╰──────────────────────────────────────────────────────╯
 ),
@@ -66,15 +66,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
        KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F12,
       KC_CAPS, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-      _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR,  KC_BTN4,  KC_BTN5,  KC_LBRC, KC_RBRC, KC_SCLN, KC_UNDS, KC_BSLS, _______,
+      _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR,  KC_MPLY,  MS_BTN3,  KC_LBRC, KC_RBRC, KC_SCLN, KC_UNDS, KC_BSLS, _______,
                _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______
  // ╰──────────────────────────────────────────────────────╯                   ╰──────────────────────────────────────────────────────╯
 ),
 [_RAISE] = LAYOUT(
  // ╭──────────────────────────────────────────────────────╮                   ╭──────────────────────────────────────────────────────╮
       _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
-       KC_ESC, KC_PSCR, KC_SCRL, KC_PAUS, XXXXXXX, G(KC_L),                      KC_HOME, KC_PGUP,   KC_UP, KC_PGDN,  KC_INS, KC_BSPC,
-       KC_TAB, KC_LALT, KC_LCTL, KC_LSFT,G(S(KC_S)),KC_CAPS,                      KC_END, KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC,
+       KC_ESC, KC_PSCR, KC_SCRL, KC_PAUS, XXXXXXX, G(KC_L),                       KC_DEL, KC_PGUP,   KC_UP, KC_PGDN,  KC_INS, KC_HOME,
+       KC_TAB, KC_LALT, KC_LCTL, KC_LSFT,G(S(KC_S)),KC_CAPS,                     KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL,  KC_END,
       _______, C(KC_Z), C(KC_X), C(KC_C), G(KC_V),G(KC_SPC), _______,  _______, KC_LSTRT,KC_PRVWD,KC_NXTWD, KC_LEND, XXXXXXX, _______,
                 _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______
  // ╰──────────────────────────────────────────────────────╯                   ╰──────────────────────────────────────────────────────╯
