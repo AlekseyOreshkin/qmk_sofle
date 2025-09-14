419ed1ced74bce8f70e7a2d486e2251680500b76
ALNOR-HPC\alnor <alxrsk@gmail.com>
2025-09-14 18:29:40 +0300

    swap encoders

--- keyboards/sofle/keymaps/custom/keymap.c
+++ keyboards/sofle/keymaps/custom/keymap.c
@@ -4,8 +4,8 @@
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLU, MS_WHLD) },
-    [1] = { ENCODER_CCW_CW(KC_MPRV, KC_MNXT), ENCODER_CCW_CW(MS_WHLL, MS_WHLR) },
+    [0] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [1] = { ENCODER_CCW_CW(MS_WHLL, MS_WHLR), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
     [2] = { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
     [3] = { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
 };
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_RBRC,
           KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_LBRC,
           KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-          KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_MUTE,  MS_BTN2,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+          KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  MS_BTN2,  KC_MUTE,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
                  KC_LCTL, KC_LALT, TD_GUI, TL_LOWR,  KC_SPC,                       KC_ENT, TL_UPPR, TD_APP, KC_RALT, KC_RCTL
   // ╰──────────────────────────────────────────────────────╯                   ╰──────────────────────────────────────────────────────╯
 ),
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
        KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F12,
       KC_CAPS, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-      _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR,  KC_MPLY,  MS_BTN3,  KC_LBRC, KC_RBRC, KC_ACPP, KC_UNDS, KC_BSLS, _______,
+      _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR,  MS_BTN3,  KC_MPLY,  KC_LBRC, KC_RBRC, KC_ACPP, KC_UNDS, KC_BSLS, _______,
                _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______
  // ╰──────────────────────────────────────────────────────╯                   ╰──────────────────────────────────────────────────────╯
 ),
