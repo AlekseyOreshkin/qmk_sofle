007e249219bc595ae0d17ef11a802a2f4df628a6
ALNOR-HPC\alnor <alxrsk@gmail.com>
2026-03-06 11:25:13 +0300

    app replaced by lower

--- keyboards/sofle/keymaps/custom/keymap.c
+++ keyboards/sofle/keymaps/custom/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_LBRC,
           KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
           KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  MS_BTN2,  KC_MUTE,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                 KC_LCTL, KC_LALT, TD_GUI, TL_LOWR,  KC_SPC,                       KC_ENT, TL_UPPR, TD_APP, KC_RALT, KC_RCTL
+                 KC_LCTL, KC_LALT, TD_GUI, TL_LOWR,  KC_SPC,                       KC_ENT,TL_UPPR, TL_LOWR, KC_RALT, KC_RCTL
   // ╰──────────────────────────────────────────────────────╯                   ╰──────────────────────────────────────────────────────╯
 ),
 [_GAME] = LAYOUT(
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_ESC,    KC_T,   KC_Q,   KC_W,    KC_E,    KC_R,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_LBRC,
           KC_TAB,    KC_G,   KC_A,   KC_S,    KC_D,    KC_F,                        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
           KC_LSFT,   KC_B,   KC_Z,   KC_X,    KC_C,    KC_V,  MS_BTN2,  KC_MUTE,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                 KC_LCTL, KC_LALT, TD_GUI, TL_LOWR,  KC_SPC,                       KC_ENT, TL_UPPR, TD_APP, KC_RALT, KC_RCTL
+                 KC_LCTL, KC_LALT, TD_GUI, TL_LOWR,  KC_SPC,                       KC_ENT,TL_UPPR, TL_LOWR, KC_RALT, KC_RCTL
   // ╰──────────────────────────────────────────────────────╯                   ╰──────────────────────────────────────────────────────╯
 ),
 [_LOWER] = LAYOUT(
